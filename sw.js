c280f9831afac1a64a6c602c051b44ba41867856
Yomi <yomismtz@gmail.com>
2026-09-16 22:11:58 -0600

    v0.2: renovar caché offline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ortopedia-dental-v01';
+const CACHE = 'ortopedia-dental-v02';
 const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
